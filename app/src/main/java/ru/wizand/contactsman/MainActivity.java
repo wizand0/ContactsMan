237c226290fee3b305a66fb515d75d8934eba02c
wizand0 <xabor@yandex.ru>
2024-08-24 12:17:41 +0300

    logic

--- app/src/main/java/ru/wizand/contactsman/MainActivity.java
+++ app/src/main/java/ru/wizand/contactsman/MainActivity.java
@@ -1,24 +1,88 @@
 package ru.wizand.contactsman;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.widget.Toolbar;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.DefaultItemAnimator;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.util.ArrayList;
+
+import ru.wizand.contactsman.adapter.ContactsAdapter;
+import ru.wizand.contactsman.db.DatabaseHelper;
+import ru.wizand.contactsman.db.entity.Contact;
 
 public class MainActivity extends AppCompatActivity {
+
+    // Variables
+    private ContactsAdapter contactsAdapter;
+    private ArrayList<Contact> contactArrayList = new ArrayList<>();
+    private RecyclerView recyclerView;
+    private DatabaseHelper db;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        // Toolbar
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("Contacts Manager");
+
+        // RecyclerView
+        recyclerView = findViewById(R.id.recycler_view_contacts);
+        db = new DatabaseHelper(this);
+
+        // Contacts List
+        contactArrayList.addAll(db.getAllContacts());
+
+        contactsAdapter = new ContactsAdapter(this, contactArrayList, MainActivity.this);
+
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getApplicationContext());
+        recyclerView.setLayoutManager(layoutManager);
+
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
+        recyclerView.setAdapter(contactsAdapter);
+
+        FloatingActionButton fab = (FloatingActionButton)  findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                addAndEditContacts(false, null, -1);
+            }
         });
+
+
+
+    }
+
+    private void addAndEditContacts(final boolean isUpdated, final Contact contact, final int position) {
+        LayoutInflater layoutInflater = LayoutInflater.from.(getApplicationContext());
+        View view = layoutInflater.inflate(R.layout.layout_add_contact,null);
+
+        AlertDialog.Builder alertDialog =  new AlertDialog.Builder(MainActivity.this);
+        alertDialog.setView(view);
+
+        TextView contactTitle = view.findViewById(R.id.new_contact_title);
+        final EditText newContact = view.findViewById(R.id.name);
+
+
+
     }
 }
---
+++ app/src/main/java/ru/wizand/contactsman/adapter/ContactsAdapter.java
@@ -0,0 +1,77 @@
+package ru.wizand.contactsman.adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+import ru.wizand.contactsman.MainActivity;
+import ru.wizand.contactsman.R;
+import ru.wizand.contactsman.db.entity.Contact;
+
+public class ContactsAdapter extends RecyclerView.Adapter<ContactsAdapter.MyViewHolder> {
+
+    //1 - variables
+    private Context context;
+    private ArrayList<Contact> contactArrayList;
+    private MainActivity mainActivity;
+
+    //2 - ViewhOLDER
+    public class MyViewHolder extends RecyclerView.ViewHolder {
+        public TextView name;
+        public TextView email;
+        public TextView phone;
+
+        public MyViewHolder(@NonNull View itemView) {
+            super(itemView);
+            this.name = itemView.findViewById(R.id.name);
+            this.email = itemView.findViewById(R.id.email);
+            this.phone = itemView.findViewById(R.id.phone);
+        }
+    }
+
+    public ContactsAdapter(Context context, ArrayList<Contact> contacts, MainActivity mainActivity) {
+        this.context = context;
+        this.contactArrayList = contacts;
+        this.mainActivity = mainActivity;
+
+    }
+
+    @NonNull
+    @Override
+    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View itemView = LayoutInflater.from(parent.getContext()).
+                inflate(R.layout.contact_list_item,parent, false);
+        return new MyViewHolder(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MyViewHolder holder,int positions) {
+        final Contact contact = contactArrayList.get(positions);
+
+        holder.name.setText(contact.getName());
+        holder.email.setText(contact.getEmail());
+        holder.phone.setText(contact.getPhone());
+
+        holder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                    mainActivity.addAndEditContacts(true, contact, positions);
+            }
+        });
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return  contactArrayList.size();
+    }
+
+
+}
